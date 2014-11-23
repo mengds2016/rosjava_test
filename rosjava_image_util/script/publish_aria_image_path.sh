d842e44832ae59646db05bbca44f5cc6577ffb1d
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2014-11-23 13:36:20 +0900

    use arg as topic name

--- rosjava_image_util/script/publish_aria_image_path.sh
+++ rosjava_image_util/script/publish_aria_image_path.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
 
-rostopic pub -r 1 /sensor_image_node/left/command/string std_msgs/String "`rospack find rosjava_test`/rosjava_image_util/img/aria.jpg" ;
+TOPIC_NAME="/aria_image/path std_msgs/String";
+if [ "$1" ];
+then
+    TOPIC_NAME=$1;
+fi;
+
+rostopic pub -r 1 $TOPIC_NAME "`rospack find rosjava_test`/rosjava_image_util/img/aria.jpg" ;
