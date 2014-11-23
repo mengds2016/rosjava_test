0dcf49ba1438019eb07f97801d82123819cd99c3
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2014-11-23 19:48:16 +0900

    chnage publish rate more slower

--- rosjava_image_util/script/publish_aria_image_path.sh
+++ rosjava_image_util/script/publish_aria_image_path.sh
@@ -6,4 +6,4 @@ TOPIC_NAME="/eus_animeface/detect_request/string";
 #     TOPIC_NAME=$1;
 # fi;
 
-rostopic pub -r 1 $TOPIC_NAME std_msgs/String "`rospack find rosjava_test`/rosjava_image_util/img/aria.jpg" ;
+rostopic pub -r 0.1 $TOPIC_NAME std_msgs/String "`rospack find rosjava_test`/rosjava_image_util/img/aria.jpg" ;
