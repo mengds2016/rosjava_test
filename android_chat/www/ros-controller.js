9e9522bb2bb391dc10529a9523481c3c8a9bf87e
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2014-12-04 06:46:35 +0900

    change laytou

--- android_chat/www/ros-controller.js
+++ android_chat/www/ros-controller.js
@@ -14,8 +14,23 @@ var ros_controller = function(opt) {
         var msg = new ROSLIB.Message(
             {data: [v,r]}
         );
+	console.log("drive [" + v + "," + r + "]");
         this.drive_topic.publish(msg);
+    };
+
+    ros_controller.prototype.publish_string_command = function(str){
     };
 };
 
 var rc = new ros_controller() ;
+
+function insert_container(command_list){
+    var elem = document.getElementById("test_container");
+    var i=0;
+    var buf="";
+    for ( ; i<command_list.length; i++ ){
+	buf += "<input type=\"image\" src=\"./test.jpg\" name=\"" + command_list[i] + "\" value=\"forward\" onclick=\"rc.publish_string_command(" + command_list[i] + ");\"></input>" ;
+    }
+    elem.innerHTML=buf;
+}
+
