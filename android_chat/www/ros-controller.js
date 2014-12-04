89106519f35a5e1cc6fc2bbe7df92abec67246da
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2014-12-05 03:50:39 +0900

    publish string command if click aria images

--- android_chat/www/ros-controller.js
+++ android_chat/www/ros-controller.js
@@ -10,6 +10,12 @@ var ros_controller = function(opt) {
         messageType: 'std_msgs/Float32MultiArray'
     });
 
+    this.string_topic = new ROSLIB.Topic({
+        ros: this.ros,
+        name: "/aria/commandline",
+        messageType: 'std_msgs/String'
+    });
+
     ros_controller.prototype.publish_drive_command = function(v,r){
         var msg = new ROSLIB.Message(
             {data: [v,r]}
@@ -19,12 +25,17 @@ var ros_controller = function(opt) {
     };
 
     ros_controller.prototype.publish_string_command = function(str){
+        var msg = new ROSLIB.Message(
+            {data: str}
+        );
+	console.log("command line " + str);
+        this.string_topic.publish(msg);
     };
 };
 
 var rc = new ros_controller() ;
 
-function insert_container(tag, container, command_list){
+function insert_container(tag, container, command_list, topic_list){
     var elem = document.getElementById(container);
     var i=0;
     var buf="";
@@ -33,7 +44,7 @@ function insert_container(tag, container, command_list){
     } else {
 	elem.title = tag;
 	for ( ; i<command_list.length; i++ ){
-	    buf += "<input type=\"image\" src=\"./test.jpg\" name=\"" + command_list[i] + "\" value=\"forward\" onclick=\"rc.publish_string_command(" + command_list[i] + ");\"></input>" ;
+	    buf += "<input type=\"image\" src=\"" + command_list[i] + "\" name=\"" + command_list[i] + "\" value=\"forward\" onclick=\"rc.publish_string_command('" + topic_list[i] + "');\"></input>" ;
 	}
     }
     elem.innerHTML=buf;
