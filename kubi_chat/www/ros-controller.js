dad14bf13c48a6c75abd3cfa966405078906ed15
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-02-18 02:40:41 +0900

    add publish_status_data function

--- kubi_chat/www/ros-controller.js
+++ kubi_chat/www/ros-controller.js
@@ -5,8 +5,7 @@ var ros_controller = function(opt) {
 
     this.drive_topic = new ROSLIB.Topic({
         ros: this.ros,
-        name: "/invented_pendulum/torque_offset_vector/once",
-	// "/chat/request/drive_vector",
+        name: "/aria/color",
         messageType: 'std_msgs/Float32MultiArray'
     });
 
@@ -50,3 +49,40 @@ function insert_container(tag, container, command_list, topic_list){
     elem.innerHTML=buf;
 }
 
+var prev_head_color="FFFFFF";
+var prev_cheek_color="FFFFFF";
+function publish_status_data(){
+    var head = document.getElementById("head_color");
+    if ( head == null ) return;
+    if ( head.value != prev_head_color ){
+	prev_head_color = head.value;
+	var red = parseInt(head.value.substr(0,2),16);
+	var grn = parseInt(head.value.substr(2,4),16);
+	var blu = parseInt(head.value.substr(4,6),16);
+	var msg = new ROSLIB.Message(
+            {data: [0,3,red,grn,blu]}
+	);
+	rc.drive_topic.publish(msg);
+    }
+    //
+    var cheek = document.getElementById("hope_color");
+    if ( cheek.value != prev_cheek_color ){
+	prev_cheek_color = cheek.value;
+	var red = parseInt(cheek.value.substr(0,2),16);
+	var grn = parseInt(cheek.value.substr(2,4),16);
+	var blu = parseInt(cheek.value.substr(4,6),16);
+	var msg = new ROSLIB.Message(
+            {data: [3,5,red,grn,blu]}
+	);
+	rc.drive_topic.publish(msg);
+    }
+    //
+    var toggle="toggle:";
+    toggle += document.getElementById("neck_checkbox").checked ? "o" : "x";
+    toggle += document.getElementById("eye_checkbox").checked ? "o" : "x";
+    toggle += document.getElementById("mimi_checkbox").checked ? "o" : "x";
+    toggle += document.getElementById("cup_checkbox").checked ? "o" : "x";
+    rc.publish_string_command(toggle);
+}
+
+setInterval('publish_status_data()',500);
